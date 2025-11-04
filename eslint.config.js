9bef17724f8b3ce4d9df96a2d06acec7fa545c55
Dave Mackey <dave@davemackey.net>
2025-11-04 13:13:03 -0500

    Add eslint

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  prettier,
+  {
+    // Ignore config file and HTML files
+    ignores: ['eslint.config.js', '**/*.html', 'node_modules/**'],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        L: 'readonly', // Leaflet global
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      // Allow console for debugging
+      'no-console': 'off',
+    },
+  }
+];
+
